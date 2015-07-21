d271288cabde17ed9109304c9bef5d6224e0d413
Pradeep Aramalla <pradeep_aramalla@jdpa.com>
2015-07-21 16:48:09 -0700

    Project Structure

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nowDoWhatUiApp
+ * @description
+ * # nowDoWhatUiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nowDoWhatUiApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nowDoWhatUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nowDoWhatUiApp
+ */
+angular.module('nowDoWhatUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
